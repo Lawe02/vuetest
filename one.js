f8277374b995b3da997c02a9586dcd8df5d4593b
Lawe Zangena <lawza02@gmail.com>
2022-01-20 14:51:00 +0100

    done with demo

---
+++ one.js
@@ -0,0 +1,10 @@
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'learn man'},
+            { text: 'äte sjnupp'},
+            { text: 'gimmie mwekf'}
+        ]
+    }
+  })
